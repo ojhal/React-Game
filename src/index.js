2e3d000fa684c6b1bae55a01740ee6206bbc6e34
Ojhal Patwa <patkarojhal26@gmail.com>
2021-01-11 13:33:11 +0530

    use setState

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+      <button className="square"
+        onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
       </button>
     );
   }
