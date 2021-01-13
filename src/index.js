b0cef73cca691c3d6282b429ccf870b4ac7623cb
Ojhal Patwa <patkarojhal26@gmail.com>
2021-01-13 15:49:48 +0530

    use functionality

--- src/index.js
+++ src/index.js
@@ -26,7 +26,12 @@ class Board extends React.Component {
     };
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
