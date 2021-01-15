398213234108ba7e4f8688b442b8faee3d7304f4
Ojhal Patwa <patkarojhal26@gmail.com>
2021-01-15 23:22:05 +0530

    use handleClick

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
       <button className="square"
